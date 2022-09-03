2af8eafc988456c4f1a3f22b38702cff587188ec
Lalit Bodana <lalitboda@gmail.com>
2022-09-03 17:33:14 +0530

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './core/components/home/home.component';
+import { AboutComponent } from './core/components/about/about.component';
+import { HeaderComponent } from './core/components/header/header.component';
+import { NotificationComponent } from './core/components/notification/notification.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    HeaderComponent,
+    NotificationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
